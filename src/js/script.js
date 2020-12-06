6ea67678490e2b4957a7dd496400539212123480
kherma <kamil.herma01@gmail.com>
2020-12-06 21:42:47 +0100

    Update header

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,29 @@
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar");
-const links = document.querySelectorAll(".nav-items li");
+const heroBtn = document.querySelector(".reveal");
+let toogler = 0;
+if (window.screen.width <= 767) {
+  navbar.classList.add("close");
+}
 
-links.forEach((link) => {
-  link.addEventListener("click", function (event) {
-    console.log("click");
-  });
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("close");
 });
 
-hamburger.addEventListener("click", function (event) {
-  navbar.classList.toggle("close");
+heroBtn.addEventListener("click", function (event) {
+  if (toogler === 0) {
+    document.querySelector(".main-title").style.visibility = "hidden";
+    document.querySelector(
+      ".header"
+    ).style.background = `url("../../images/monster-3.JPG") no-repeat center center/cover`;
+    event.target.textContent = "click to hide the true";
+    toogler = 1;
+  } else {
+    document.querySelector(".main-title").style.visibility = "visible";
+    document.querySelector(
+      ".header"
+    ).style.background = `url("../../images/hero-2.JPG") no-repeat center center/cover`;
+    event.target.textContent = "click to see the true";
+    toogler = 0;
+  }
 });
